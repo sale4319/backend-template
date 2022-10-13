7f9a2cacea258fc575b2908fae47c499dda3d8d1
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-10-13 11:36:31 +0200

    initial commit

---
+++ src/main/java/com/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.model.User;
+import com.backend.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
