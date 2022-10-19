510c6ae285dbba6ffc7b1bdb617a0f6c39f47926
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-10-19 12:53:14 +0200

    dependency: added lombok

--- src/main/java/com/backend/model/User.java
+++ src/main/java/com/backend/model/User.java
@@ -1,10 +1,17 @@
 package com.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
@@ -12,37 +19,5 @@ public class User {
     private String username;
     private String name;
     private String email;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
